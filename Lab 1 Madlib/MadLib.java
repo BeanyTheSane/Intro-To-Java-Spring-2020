2044c651bd47cb01671eba55356e3192d9b81479
BeanyTheSane <adam.knitter@emerson.com>
2020-01-22 15:30:46 -0500

    it works

--- Lab 1 Madlib/MadLib.java
+++ Lab 1 Madlib/MadLib.java
@@ -1,19 +1,31 @@
+import java.util.Random;
 public class MadLib {
-
     public static void main(String[] args) {
         
-        String[] people = {"Luke Skywalker", "Princess Leia", "Han Solo", "Obi-Wan Kenobi", "Darth Vader", "Emperor Palpatine", "Darth Maul", "Jabba the Hutt"};
-        String[] places = {"The Death Star", "Alderaan", "Endor", "Hoth", "Tatooine"};
-        String[] weapons = {"Lightsaber", "Bowcaster", "Blaster", "Force Lightning", "Thermal Detonator"};
-
-        int peopleCount = people.length;
-        int placesCount = places.length;
-        int weaponsCount = weapons.length;
-
-        int peopleRandomIndex = 0;
-        int placesRandomIndex = 0;
-        int weaponsRandomIndex = 0;
-
         
+        String[] protagonists = {"Luke Skywalker", "Princess Leia", "Han Solo", "Obi-Wan Kenobi"}; 
+        String[] antagonists = {"Darth Vader", "Emperor Palpatine", "Darth Maul", "Jabba the Hutt"};
+        String[] places = {"The Death Star", "Alderaan", "Endor", "Hoth", "Tatooine"};
+        String[] weapons = {"Lightsaber", "Bowcaster", "Blaster", "Blast of Force Lightning", "Thermal Detonator"};
+
+        String randomProtagonist = getRandom(protagonists);
+        String randomAntagonist = getRandom(antagonists);
+        String randomPlace = getRandom(places);
+        String randomWeapon = getRandom(weapons);
+
+        String completedTextToDisplay = randomProtagonist 
+                                      + " attacked " 
+                                      + randomAntagonist 
+                                      + " with a " 
+                                      + randomWeapon
+                                      + " on "
+                                      + randomPlace;
+
+        System.out.println(completedTextToDisplay);
+    }
+
+    public static String getRandom(String[] array) {
+        int rnd = new Random().nextInt(array.length);
+        return array[rnd];
     }
 }
