84da6f89e5ae1e5a580ce8ca108125023da7308e
BeanyTheSane <adam.knitter@emerson.com>
2020-01-22 14:13:47 -0500

    Creating variables

---
+++ Lab 1 Madlib/MadLib.java
@@ -0,0 +1,11 @@
+public class MadLib {
+
+    public static void main(String[] args) {
+        
+        String[] people = {"Luke Skywalker", "Princess Leia", "Han Solo", "Obi-Wan Kenobi", "Darth Vader", "Emperor Palpatine", "Darth Maul", "Jabba the Hutt"};
+        String[] places = {"The Death Star", "Alderaan", "Endor", "Hoth", "Tatooine"};
+        String[] things = {"Lightsaber", "Bowcaster", "Blaster", "Force Lightning", "Thermal Detonator"};
+
+
+    }
+}
