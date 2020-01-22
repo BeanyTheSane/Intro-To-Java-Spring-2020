6d94fdeb7c3a9715472db2768207a12529b596c9
BeanyTheSane <adam.knitter@emerson.com>
2020-01-22 16:22:40 -0500

    Factored out objects

--- Lab 1 Madlib/MadLib.java
+++ Lab 1 Madlib/MadLib.java
@@ -1,31 +1,49 @@
 import java.util.Random;
 public class MadLib {
     public static void main(String[] args) {
-        
-        
-        String[] protagonists = {"Luke Skywalker", "Princess Leia", "Han Solo", "Obi-Wan Kenobi"}; 
-        String[] antagonists = {"Darth Vader", "Emperor Palpatine", "Darth Maul", "Jabba the Hutt"};
-        String[] places = {"The Death Star", "Alderaan", "Endor", "Hoth", "Tatooine"};
-        String[] weapons = {"Lightsaber", "Bowcaster", "Blaster", "Blast of Force Lightning", "Thermal Detonator"};
 
-        String randomProtagonist = getRandom(protagonists);
-        String randomAntagonist = getRandom(antagonists);
-        String randomPlace = getRandom(places);
-        String randomWeapon = getRandom(weapons);
+        System.out.println(textToDisplayBuilder());
 
-        String completedTextToDisplay = randomProtagonist 
-                                      + " attacked " 
-                                      + randomAntagonist 
-                                      + " with a " 
-                                      + randomWeapon
-                                      + " on "
-                                      + randomPlace;
-
-        System.out.println(completedTextToDisplay);
+    }
+    
+    public static String textToDisplayBuilder() {
+        WordFillInModel wordFillIns = new WordFillInModel();
+        return   wordFillIns.getRandomProtagonist()
+               + " attacked "
+               + wordFillIns.getRandomAntagonist()
+               + " with a "
+               + wordFillIns.getRandomWeapon()
+               + " on "
+               +wordFillIns.getRandomPlace();
     }
 
     public static String getRandom(String[] array) {
         int rnd = new Random().nextInt(array.length);
         return array[rnd];
+    }
+
+    public static class WordFillInModel {
+        String[] protagonists = {"Luke Skywalker", "Princess Leia", "Han Solo", "Obi-Wan Kenobi"}; 
+        String[] antagonists = {"Darth Vader", "Emperor Palpatine", "Darth Maul", "Jabba the Hutt"};
+        String[] places = {"The Death Star", "Alderaan", "Endor", "Hoth", "Tatooine"};
+        String[] weapons = {"Lightsaber", "Bowcaster", "Blaster", "Blast of Force Lightning", "Thermal Detonator"};
+
+        void wordFillInModel() {}
+
+        String getRandomProtagonist() {
+            return getRandom(this.protagonists);
+        }
+
+        String getRandomAntagonist() {
+            return getRandom(this.antagonists);
+        }
+
+        String getRandomPlace() {
+            return getRandom(this.places);
+        }
+
+        String getRandomWeapon() {
+            return getRandom(this.weapons);
+        }
     }
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# LCCC-Spring-2020
-A place to store my school work for spring 2020 
