bc2af7080ed03b70f8c357b675f8e4ec712e5ba8
BeanyTheSane <adam.knitter@emerson.com>
2020-01-22 14:17:08 -0500

    Creating more variables and testing git in terminal

--- Lab 1 Madlib/MadLib.java
+++ Lab 1 Madlib/MadLib.java
@@ -4,8 +4,16 @@ public class MadLib {
         
         String[] people = {"Luke Skywalker", "Princess Leia", "Han Solo", "Obi-Wan Kenobi", "Darth Vader", "Emperor Palpatine", "Darth Maul", "Jabba the Hutt"};
         String[] places = {"The Death Star", "Alderaan", "Endor", "Hoth", "Tatooine"};
-        String[] things = {"Lightsaber", "Bowcaster", "Blaster", "Force Lightning", "Thermal Detonator"};
+        String[] weapons = {"Lightsaber", "Bowcaster", "Blaster", "Force Lightning", "Thermal Detonator"};
 
+        int peopleCount = people.length;
+        int placesCount = places.length;
+        int weaponsCount = weapons.length;
 
+        int peopleRandomIndex = 0;
+        int placesRandomIndex = 0;
+        int weaponsRandomIndex = 0;
+
+        
     }
 }
