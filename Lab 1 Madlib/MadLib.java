7befa9b8702dbdfa9a08d3edc52c2c34e0187cf1
Adam Knitter <knitter0288@gmail.com>
2020-01-23 23:51:05 -0500

    Added getters and setters, factored code into more obscure but descriptive methods

--- Lab 1 Madlib/MadLib.java
+++ Lab 1 Madlib/MadLib.java
@@ -8,13 +8,22 @@ public class MadLib {
     
     public static String textToDisplayBuilder() {
         WordFillInModel wordFillIns = new WordFillInModel();
+        wordFillIns.useDefaultLists();
         return   wordFillIns.getRandomProtagonist()
                + " attacked "
                + wordFillIns.getRandomAntagonist()
                + " with a "
                + wordFillIns.getRandomWeapon()
                + " on "
-               +wordFillIns.getRandomPlace();
+               + wordFillIns.getRandomPlace()
+               + "\n\n"
+               + wordFillIns.getProtagonists()
+               + "\n\n"
+               + wordFillIns.getAntagonists()
+               + "\n\n"
+               + wordFillIns.getPlaces()
+               + "\n\n"
+               + wordFillIns.getWeapons();
     }
 
     public static String getRandom(String[] array) {
@@ -23,12 +32,81 @@ public class MadLib {
     }
 
     public static class WordFillInModel {
-        String[] protagonists = {"Luke Skywalker", "Princess Leia", "Han Solo", "Obi-Wan Kenobi"}; 
-        String[] antagonists = {"Darth Vader", "Emperor Palpatine", "Darth Maul", "Jabba the Hutt"};
-        String[] places = {"The Death Star", "Alderaan", "Endor", "Hoth", "Tatooine"};
-        String[] weapons = {"Lightsaber", "Bowcaster", "Blaster", "Blast of Force Lightning", "Thermal Detonator"};
+        private String[] protagonists = {}; 
+        private String[] antagonists = {};
+        private String[] places = {};
+        private String[] weapons = {};
 
-        void wordFillInModel() {}
+        void wordFillInModel() {
+        }
+
+        void useDefaultLists() {
+            String[] protagonists = {"Luke Skywalker", "Princess Leia", "Han Solo", "Obi-Wan Kenobi"}; 
+            String[] antagonists = {"Darth Vader", "Emperor Palpatine", "Darth Maul", "Jabba the Hutt"};
+            String[] places = {"The Death Star", "Alderaan", "Endor", "Hoth", "Tatooine"};
+            String[] weapons = {"Lightsaber", "Bowcaster", "Blaster", "Blast of Force Lightning", "Thermal Detonator"};
+
+            setProtagonists(protagonists);
+            setAntagonists(antagonists);
+            setPlaces(places);
+            setWeapons(weapons);
+        }
+
+        String getProtagonists() {
+           String protagonists = "";
+
+            for (String person : this.protagonists) {
+                protagonists += person + ", ";
+            }
+
+           return protagonists;
+        }
+
+        void setProtagonists(String[] newProtagonists) {
+            this.protagonists = newProtagonists;
+        }
+
+        String getAntagonists() {
+           String antagonists = "";
+
+            for (String person : this.antagonists) {
+                antagonists += person + ", ";
+            }
+
+           return antagonists;
+        }
+
+        void setAntagonists(String[] newProtagonists) {
+            this.antagonists = newProtagonists;
+        }
+
+        String getPlaces() {
+           String places = "";
+
+            for (String person : this.places) {
+                places += person + ", ";
+            }
+
+           return places;
+        }
+
+        void setPlaces(String[] newProtagonists) {
+            this.places = newProtagonists;
+        }
+
+        String getWeapons() {
+           String weapons = "";
+
+            for (String person : this.weapons) {
+                weapons += person + ", ";
+            }
+
+           return weapons;
+        }
+
+        void setWeapons(String[] newProtagonists) {
+            this.weapons = newProtagonists;
+        }
 
         String getRandomProtagonist() {
             return getRandom(this.protagonists);
@@ -44,6 +122,34 @@ public class MadLib {
 
         String getRandomWeapon() {
             return getRandom(this.weapons);
+        }
+
+        void addWeapons(String person) {
+            addItem(weapons, person);
+        }
+
+        void addPlaces(String person) {
+            addItem(places, person);
+        }
+
+        void addAntagonist(String person) {
+            addItem(antagonists, person);
+        }
+
+        void addProtagonist(String person) {
+            addItem(protagonists, person);
+        }
+
+        String[] addItem(String initialArray[], String itemToAdd) {
+            String newArray[] = new String[initialArray.length + 1];
+
+            for (int i = 0; i < initialArray.length; i++) {
+                newArray[i] = initialArray[i];
+            }
+
+            newArray[initialArray.length] = itemToAdd;
+
+            return newArray;
         }
     }
 }
