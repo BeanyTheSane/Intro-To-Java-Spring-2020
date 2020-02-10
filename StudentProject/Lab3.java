8a1de85d9876a5e3143681e47afa3c8045901e73
BeanyTheSane <adam.knitter@emerson.com>
2020-02-10 16:40:03 -0500

    Added tests for new constructors

--- StudentProject/Lab3.java
+++ StudentProject/Lab3.java
@@ -1,18 +1,27 @@
+import java.util.Scanner;
+
 public class Lab3 {
 
     public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        Student constructorTest1 = new Student("Cloud Strife", "999999", 12, "OOC");
+        Student constructorTest2 = new Student("Cait Sith", "111111", 4);
+
+        System.out.println("\nThe following two Students are created using the new constructors\n");
+        System.out.println(constructorTest1.getDetailsAsString());
+        System.out.println(constructorTest2.getDetailsAsString());
+
+        System.out.println("\nPress any Key to run the full tuition chart test\n");
+        myScanner.nextLine();
 
         runTuitionTest();
-        
+
+        System.out.println("\nPress any Key to Exit\n");
+        myScanner.nextLine();
     }
 
     private static void runTuitionTest() {
         int testCaseLimit = 22;//general max number of credit hours reccomended
-        // ArrayList<String> listOfInCountyStudents = new ArrayList<>();
-        // ArrayList<String> listOfOutOfCountyStudents = new ArrayList<>();
-        // ArrayList<String> listOfOutOfStateStudents = new ArrayList<>();
-
-        //add lists to store each set as they are generated
 
         for (int i = 1; i <= testCaseLimit; i++) {
             Student inCountyStudent = new Student();
@@ -21,18 +30,10 @@ public class Lab3 {
             inCountyStudent.buildRandomPerson("INC", i);
             outOfCountyStudent.buildRandomPerson("OOC", i);
             outOfStateStudent.buildRandomPerson("OOS", i);
-            //build a method to output the data of these students on Student in the format that I need (see canvas for format)
-            //I don"t really like the way the following works, we have to create a second loop to unwind these lists, lets just print them out now.
-            // listOfInCountyStudents.add(inCountyStudent.getDetailsAsString());
-            // listOfOutOfCountyStudents.add(outOfCountyStudent.getDetailsAsString());
-            // listOfOutOfStateStudents.add(outOfStateStudent.getDetailsAsString());
 
             System.out.println(inCountyStudent.getDetailsAsString());
             System.out.println(outOfCountyStudent.getDetailsAsString());
             System.out.println(outOfStateStudent.getDetailsAsString());
         }
-
-
-
     }
 }
