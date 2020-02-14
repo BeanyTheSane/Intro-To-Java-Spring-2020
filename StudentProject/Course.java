e4859c1b37b12480b13444f814db22e3d6abc577
BeanyTheSane <adam.knitter@emerson.com>
2020-02-14 09:44:01 -0500

    Added function to return all course details as a formatted string

--- StudentProject/Course.java
+++ StudentProject/Course.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Course {
     String courseId;
     String courseName;
@@ -34,4 +37,49 @@ public class Course {
         this.courseName = "Test Course";
         this.creditHours = creditHours;
     }
+    public String getCourseDetailsAsString() {
+        StringBuilder courseDetails = new StringBuilder();
+
+        courseDetails.append("Id #" + getCourseId());
+        courseDetails.append(", Name: " + getCourseName());
+        courseDetails.append(", Credit Hours: " + getCreditHours());
+
+        return courseDetails.toString();
+    }
+
+    public static ArrayList<Course> buildDefaultCourseList(int creditHours) {
+		ArrayList<Course> defaultCourseList = new ArrayList<>();
+        
+        if (creditHours == 0) {
+            //this will end up returning an empty List
+        }
+        else if (creditHours == 1) {
+			Course course = new Course();
+			course.setDefaultNameAndIdByCreditHours(1);
+			defaultCourseList.add(course);
+		}
+		else {
+			int total = 0;
+            int subTotal = 0;
+            int lastCourse = 0;
+            while (total != creditHours) {
+                int randomCreditHour = new Random().nextInt(6-1) + 1;
+                total += randomCreditHour;
+                if (total < creditHours) {
+                    Course course = new Course();
+                    course.setDefaultNameAndIdByCreditHours(randomCreditHour);
+                    defaultCourseList.add(course);
+                    subTotal += randomCreditHour;
+                }
+                else {
+                    total = creditHours;
+                    lastCourse = creditHours - subTotal;
+                    Course course = new Course();
+                    course.setDefaultNameAndIdByCreditHours(lastCourse);
+                    defaultCourseList.add(course);
+                }
+		    }
+        }
+		return defaultCourseList;
+	}
 }
