e662cef47678e9456be74bc282be0a83e783015d
BeanyTheSane <adam.knitter@emerson.com>
2020-02-27 16:58:03 -0500

    Began adding some methods to the base subscription class

--- lab6_Subscription_Service/Subscription.java
+++ lab6_Subscription_Service/Subscription.java
@@ -1,4 +1,5 @@
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.time.LocalDateTime;
 
 public class Subscription {
@@ -27,4 +28,12 @@ public class Subscription {
         this.startDate = startDate;
     }
 
+    public BigDecimal costOfRenewalRaw() {
+        return this.cost;
+    }
+
+    public String foramttedCostOfRenewal() {
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
+        return currencyFormatter.format(this.cost);
+    }
 }
